fa8726e3e16e119a0ce5f3c426d32f25c1439d3e
matiasgegundez <82420739+matiasgegundez@users.noreply.github.com>
2022-08-26 20:42:13 -0300

    Agregando ultimo endpoint

---
+++ Controllers/AplicacionController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UltimoDesafio.Model;
+using UltimoDesafio.Repository;
+
+namespace UltimoDesafio.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AplicacionController : ControllerBase
+    {
+        public const string APP_Name = "Proyecto final C# de Matias";
+        
+        [HttpGet(Name = "GetApplicationName")]
+        public string GetNombreDeApp()
+        {
+            return APP_Name;
+        }
+    }
+}
